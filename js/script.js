06ad6fa6f0980313b5d2939f6fc8ce6c562c15a4
MatteoCarbone17 <matteocarbone17@live.com>
2022-11-24 15:27:56 +0100

    number generetor in conosole log

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+*/
+
+
+
+let numberUser = Math.floor(Math.random()*7);
+let numberComputer = Math.floor(Math.random()*7);
+
+if (numberUser>numberComputer){
+    console.log('punti fatti '+ numberUser +' hai vinto!')
+    console.log('punti fatti dal pc '+ numberComputer)
+} else{
+    console.log('punti fatti dal pc '+ numberComputer)
+    console.log('punti fatti '+ numberUser +' hai perso')
+}
+
